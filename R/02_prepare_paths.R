1922f9aa40c0ccf399a5ba6deaad913ee5873878
Murray Logan <i.obesulus@gmail.com>
2024-04-20 18:05:34 +1000

    Update 02_prepare_paths.R

--- R/02_prepare_paths.R
+++ R/02_prepare_paths.R
@@ -1,4 +1,5 @@
 ## ---- prepare_paths
+if (!dir.exists("../data")) dir.create("../data")
 if (!dir.exists("../data/processed")) dir.create("../data/processed")
 if (!dir.exists("../data/modelled")) dir.create("../data/modelled")
 ## ----end
